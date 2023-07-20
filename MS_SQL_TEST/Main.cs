2b826e44413affb646b34732e67ff8c960ef86e2
mnorval <mnorval@gmail.com>
2023-07-20 16:03:34 +0200

    Add files via upload

---
+++ MS_SQL_TEST/Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace MS_SQL_TEST
+{
+    public partial class Main : Form
+    {
+
+        SqlConnection myConnection = new SqlConnection("user id=administrator;" +
+                                       "password=admin;" +
+                                       @"server=192.168.1.25\SQLEXPRESS;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=MIKE; " +
+                                       "connection timeout=30");
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenSQL();
+        }
+
+        void OpenSQL()
+        {
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception ae)
+            {
+
+                MessageBox.Show(ae.ToString());
+            }
+        }
+
+
+        void CloseSQL()
+        {
+            try
+            {
+                myConnection.Close();
+            }
+            catch (Exception ae)
+            {
+
+                MessageBox.Show(ae.ToString());
+            }
+        }
+
+    }
+}
